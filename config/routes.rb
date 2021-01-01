bb7d6b57234396b1fad496fa5f0509f033d732e5
Yuta <yuta.shogai@gmail.com>
2021-01-01 10:11:32 +0000

    [Update] Bookers 'ルート更新とトップHTMLの編集'

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/top'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :blogs
+  root :to => "books#top"
 end
