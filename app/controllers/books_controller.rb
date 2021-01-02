7d67adbb6a287c4071cf94595ee64714f77dc29f
Yuta <yuta.shogai@gmail.com>
2021-01-02 11:10:17 +0000

    [Update] Bookers 'top・indexのview、コントローラーの編集'

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,24 @@
 class BooksController < ApplicationController
-  def top
-  end
 
   def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
