f4118329fbb9b53861a3f63b185d8cc4052df3ac
Yuta <yuta.shogai@gmail.com>
2021-01-03 05:54:36 +0000

    [Update] Bookers 'index,edit,showのHTMLの編集、コントローラ編集、Bookモデルのvalidate対応、サクセスメッセージの表示'

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,14 +7,37 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+    if book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    else
+      redirect_to books_path
+    end
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    else
+      redirect_to edit_book_path(book.id)
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path
   end
 
   private
