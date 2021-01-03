d51e4e9e5ed08970c3224a0afff815d0c19c017d
Yuta <yuta.shogai@gmail.com>
2021-01-03 11:12:38 +0000

    [Updated] Bookers 'エラーメッセージの実装'

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,12 +6,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
-      redirect_to books_path
+      render :index
     end
   end
 
@@ -24,12 +25,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
-      redirect_to edit_book_path(book.id)
+      render :edit
     end
   end
 
