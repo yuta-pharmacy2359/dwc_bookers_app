9a32dfb86abbc5b95100ddca19a69a96c3fb50aa
Yuta <yuta.shogai@gmail.com>
2021-02-24 15:38:58 +0000

    [Update] マイページのリンクまで作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!, except: [:top]
 
   def index
     @books = Book.all.order(:id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/edit'
   devise_for :users
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
   root :to => "books#top"
